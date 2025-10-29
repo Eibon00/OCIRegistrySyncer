ccf02598ec5857bca22bb3da4b3e9d378de09bdf
Neboer <rubinposter@gmail.com>
2025-10-29 17:26:07 +0800

    权限问题修复

--- install-realscoped-skopeo.sh
+++ install-realscoped-skopeo.sh
@@ -6,6 +6,9 @@ set -euo pipefail
 SKOPEO_BINARY_URL="${SKOPEO_BINARY_URL:-https://github.com/Neboer/realscoped-skopeo/releases/download/v1.20.0-realscoped/skopeo-linux-amd64}"
 if [ -e "/usr/bin/skopeo" ]; then
     echo "Removing existing /usr/bin/skopeo"
-    rm -f -- "/usr/bin/skopeo"
+    sudo rm -f -- "/usr/bin/skopeo"
 fi
-curl -L --output /usr/bin/skopeo "$SKOPEO_BINARY_URL"
+sudo curl -L --output /usr/bin/skopeo "$SKOPEO_BINARY_URL"
+echo "Downloaded realscoped skopeo from $SKOPEO_BINARY_URL"
+sudo chmod +x /usr/bin/skopeo
+echo "Realscoped skopeo installed to /usr/bin/skopeo"
