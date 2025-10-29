305a1669f3aa0909d96f07c62ed46f76e17a7852
Neboer <rubinposter@gmail.com>
2025-10-29 17:24:26 +0800

    yggdrasil安装后自动启动

--- install-yggdrasil-configure-network.sh
+++ install-yggdrasil-configure-network.sh
@@ -5,6 +5,6 @@ gpg --export BC1BF63BD10B8F1A | sudo tee /usr/local/apt-keys/yggdrasil-keyring.g
 echo 'deb [signed-by=/usr/local/apt-keys/yggdrasil-keyring.gpg] http://neilalexander.s3.dualstack.eu-west-2.amazonaws.com/deb/ debian yggdrasil' | sudo tee /etc/apt/sources.list.d/yggdrasil.list
 sudo apt-get update
 sudo apt-get -y install yggdrasil
-
+sudo systemctl start yggdrasil
 sudo yggdrasilctl addpeer "$YGG_ENDPOINT"
 echo "$YGG_HOSTS"|sudo tee -a /etc/hosts > /dev/null
