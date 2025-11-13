cfd942984ba2dadb86201dfd12d879cefbcae1d8
Neboer <rubinposter@gmail.com>
2025-11-13 23:19:20 +0800

    Bugfix: Update Yggdrasil installation script
    download github release directly.

--- install-yggdrasil-configure-network.sh
+++ install-yggdrasil-configure-network.sh
@@ -1,10 +1,6 @@
 #!/bin/bash
-sudo mkdir -p /usr/local/apt-keys
-gpg --fetch-keys https://neilalexander.s3.dualstack.eu-west-2.amazonaws.com/deb/key.txt
-gpg --export BC1BF63BD10B8F1A | sudo tee /usr/local/apt-keys/yggdrasil-keyring.gpg > /dev/null
-echo 'deb [signed-by=/usr/local/apt-keys/yggdrasil-keyring.gpg] http://neilalexander.s3.dualstack.eu-west-2.amazonaws.com/deb/ debian yggdrasil' | sudo tee /etc/apt/sources.list.d/yggdrasil.list
-sudo apt-get update
-sudo apt-get -y install yggdrasil
+curl -OL https://github.com/yggdrasil-network/yggdrasil-go/releases/download/v0.5.12/yggdrasil-0.5.12-amd64.deb
+sudo DEBIAN_FRONTEND=noninteractive  apt install -y ./yggdrasil-0.5.12-amd64.deb
 yggdrasil -genconf | sudo tee /etc/yggdrasil.conf > /dev/null
 sudo systemctl start yggdrasil
 sudo yggdrasilctl addpeer "$YGG_ENDPOINT"
